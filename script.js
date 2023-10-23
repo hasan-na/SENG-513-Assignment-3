0a165b243bc16b63a019c9b28f4be0d7b10dc593
hasan-na <hasannassar39@gmail.com>
2023-10-22 18:48:46 -0600

    added draw to html and css file also added skeleton functionality to the game

---
+++ script.js
@@ -0,0 +1,60 @@
+//Skeleton for Connect4 Game
+
+//Constant that never change throughout the game
+const ROWS = 6
+const COLUMNS = 7
+const PLAYER_1 = 'red';
+const PLAYER_2 = 'yellow';
+
+//Creates the game board to play the game
+function createBoard(row, column){
+
+}
+
+//Checks to see if player moves are valid and handles player moves accordingly
+function playerMoves(row, column){
+
+}
+
+//Checks to see if someone won the game by getting fow in a row, column or diagonally
+function checkWinCondition(row, column){
+
+}
+
+//Checks to see if the game ended in a stalemate instead of someone winning
+function draw(row, column){
+
+}
+
+//Update the status above the game board to accurately portray whose turn it is and if someone won the game
+function updateStatus(){
+
+}
+
+//update the gameboard when someone makes a move
+function updateBoard(){
+
+}
+
+//add functionality to the start game button to properly start the game 
+function startGameListener(){
+
+}
+
+//add functionality to the reset game button in order to reset the game properly
+function resetGameListener(){
+
+}
+
+//add functionality that listens to which cell the user is currently clicking
+function cellClickListener(){
+    
+}
+
+
+//Initialize the Game 
+function initializeGame(){
+
+}
+
+
